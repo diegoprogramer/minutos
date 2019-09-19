b60f54d935ce3d0ea761b05e1747e6a702ca6488
diego programer <diegoalexac@gmail.com>
2019-09-18 20:34:54 -0500

    primer commit

---
+++ src/minutos/Minutos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minutos;
+
+/**
+ *
+ * @author alexander
+ */
+public class Minutos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // codigo para llamar a la ventana
+        Ventana v1=new Ventana();
+        v1.setVisible(true);
+    }
+    
+}
